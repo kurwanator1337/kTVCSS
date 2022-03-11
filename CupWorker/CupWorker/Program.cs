74dde6101485cf47cbd57a913e4df3b1a1f33750
kurwanator1337 <ktvcss@mail.ru>
2022-03-11 14:43:32 +0400

    Подготовлена рыба для CupWorker

---
+++ CupWorker/CupWorker/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace CupWorker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "kTVCSS Cup Worker";
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            using (SqlConnection connection = new SqlConnection(ProgSettings.Default.SQL))
+            {
+                connection.Open();
+                SqlCommand query = new SqlCommand($"SELECT ATEAM, BTEAM, SERVERID, DTSTART, DTEND, ID FROM CupWorkerMatches WHERE STATUS = 1", connection);
+                using (SqlDataReader reader = query.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string ATeam = reader[0].ToString();
+                        string BTeam = reader[1].ToString();
+                        string ServerID = reader[2].ToString();
+                        DateTime DTStart = DateTime.Parse(reader[3].ToString());
+                        DateTime DTEnd = DateTime.Parse(reader[4].ToString());
+                        string ID = reader[5].ToString();
+
+                        string[] server = Tools.GetServerInfo(ID);
+
+                        Thread controlThread = new Thread(Tools.MatchControl);
+                        controlThread.Start(ATeam + ";" + BTeam + ";" + server[0] + ";" + server[1] + ";" + server[2] + ";" + DTStart + ";" + DTEnd);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CupWorker/CupWorker/Tools.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CupWorker
+{
+    public static class Tools
+    {
+        public static string[] GetServerInfo(string id)
+        {
+            List<string> result = new List<string>();
+            var connection = new SqlConnection(ProgSettings.Default.SQL);
+            connection.Open();
+            var query = new SqlCommand($"SELECT HOST, GAMEPORT, RCONPASSWORD FROM GameServers WHERE ID = {id}", connection);
+            var reader = query.ExecuteReader();
+            while (reader.Read())
+            {
+                result.Add(reader[0].ToString());
+                result.Add(reader[1].ToString());
+                result.Add(reader[2].ToString());
+            }
+            connection.Close();
+            return result.ToArray();
+        }
+
+        public async static void MatchControl(object args)
+        {
+            var input = args.ToString().Split(';');
+            string tTrueName = input[0];
+            string ctTrueName = input[1];
+            string host = input[2];
+            int port = int.Parse(input[3]);
+            string password = input[4];
+            DateTime end = DateTime.Parse(input[5]);
+            string id = input[6];
+
+            RconSharp.RconClient client = RconSharp.RconClient.Create(host, port);
+            await client.ConnectAsync();
+            if (await client.AuthenticateAsync(password))
+            {
+                while (true)
+                {
+                    string OnlinePlayersQuery = await client.ExecuteCommandAsync("sm_kurwagay");
+                    //var stuffFromDb = GetSteams(tTrueName, ctTrueName);
+
+                    //foreach (var item in stuffFromServer)
+                    //{
+                    //    if (!item.Contains("STEAM")) continue;
+                    //    var steam = item.Split(';');
+                    //    if (!stuffFromDb.Contains(steam[1]))
+                    //    {
+                    //        webClient.DownloadString("https://v34.ktvcss.org.ru/rcon.php?address=" + host +
+                    //        "&port=" + port +
+                    //            "&password=" + password +
+                    //        "&command=kickid " + steam[0] + " [kTVCSSCupBot] Access on this match is denied for you");
+                    //        Console.WriteLine(DateTime.Now + ": кикаем " + steam[1] + " с сервера " + host + ":" + port);
+                    //    }
+                    if (end.Day == DateTime.Now.Day && end.Hour == DateTime.Now.Hour && end.Minute == DateTime.Now.Minute)
+                    {
+                        Console.WriteLine(DateTime.Now + ": завершаем матч " + tTrueName + " против " + ctTrueName);
+                        //DeleteMatch(id);
+                        return;
+                    }
+                }
+
+                
+
+                //Thread.Sleep(30000);
+            }
+        }
+    }
+}
