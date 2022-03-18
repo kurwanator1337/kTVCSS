59fa80d756dfb2f74b4b48f453a3b25a55cabfc1
kurwanator1337 <ktvcss@mail.ru>
2022-03-18 22:17:47 +0400

    Сделано приложение в стиле WatchDog для процессов

---
+++ WardenCat/WardenCat/ProcessInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WardenCat
+{
+    public class ProcessInfo
+    {
+        public string Path { get; set; }
+        public int Id { get; set; }
+        public string Args { get; set; }
+    }
+}
---
+++ WardenCat/WardenCat/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Management;
+using System.Text.RegularExpressions;
+
+namespace WardenCat
+{
+    internal class Program
+    {
+        private static List<ProcessInfo> ProcessList = new List<ProcessInfo>();
+
+        private static void GetProcessList()
+        {
+            string wmiQuery = string.Format("select CommandLine, ProcessId from Win32_Process where Name='{0}'", "kTVCSS.exe");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(wmiQuery);
+            ManagementObjectCollection retObjectCollection = searcher.Get();
+            foreach (ManagementObject retObject in retObjectCollection)
+            {
+                foreach (Match m in Regex.Matches(retObject["CommandLine"].ToString(), @"(?<Path>"".*?"") (?<Args>.*)", RegexOptions.Multiline))
+                {
+                    ProcessList.Add(new ProcessInfo()
+                    {
+                        Path = m.Groups["Path"].Value,
+                        Args = m.Groups["Args"].Value,
+                        Id = int.Parse(retObject["ProcessId"].ToString())
+                    });
+                }
+            }
+        }
+
+        private static void SetHook()
+        {
+            foreach (ProcessInfo process in ProcessList)
+            {
+                Process proc = Process.GetProcessById(process.Id);
+                proc.EnableRaisingEvents = true;
+
+                proc.Exited += (a, e) =>
+                {
+                    Process node = new Process();
+                    node.StartInfo.UseShellExecute = true;
+                    node.StartInfo.FileName = process.Path;
+                    node.StartInfo.Arguments = process.Args;
+                    node.Start();
+                    ProcessList.Clear();
+                    GetProcessList();
+                    SetHook();
+                };
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            GetProcessList();
+
+            SetHook();
+
+            Console.WriteLine("Started. If you need to close it press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
