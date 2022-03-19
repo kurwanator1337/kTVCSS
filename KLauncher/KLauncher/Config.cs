d50ad6be5ea4fad31bfbc70ded2fa66662082426
kurwanator1337 <ktvcss@mail.ru>
2022-03-19 22:41:38 +0400

    Добавлена программа-лаунчер всего необходимого для работы

---
+++ KLauncher/KLauncher/Config.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace KLauncher
+{
+    [XmlRoot(ElementName = "Process")]
+    public class Process
+    {
+        [XmlElement(ElementName = "Path")]
+        public string Path { get; set; }
+
+        [XmlElement(ElementName = "Args")]
+        public string Args { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Config")]
+    public class Config
+    {
+        [XmlElement(ElementName = "Process")]
+        public List<Process> Process { get; set; }
+    }
+
+    public class ConfigTools
+    {
+        public Config ProcessList = new Config();
+
+        public ConfigTools()
+        {
+            ProcessList = new Config();
+            XmlSerializer serializer = new XmlSerializer(typeof(Config));
+            using (StringReader reader = new StringReader(File.ReadAllText("kTVCSS.cfg")))
+            {
+                ProcessList = (Config)serializer.Deserialize(reader);
+            }
+        }
+    }
+}
---
+++ KLauncher/KLauncher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KLauncher
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ConfigTools config = new ConfigTools();
+
+            foreach (var item in config.ProcessList.Process)
+            {
+                System.Diagnostics.Process node = new System.Diagnostics.Process();
+                node.StartInfo.UseShellExecute = true;
+                node.StartInfo.FileName = item.Path;
+                node.StartInfo.Arguments = item.Args;
+                node.Start();
+            }
+        }
+    }
+}
